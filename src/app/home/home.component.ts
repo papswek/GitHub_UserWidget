4913a1cabc9274d031d2ce45b6fb0d64a578aa5b
papswek <97041912+papswek@users.noreply.github.com>
2023-02-19 04:42:02 +0800

    1 file modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,9 +13,23 @@ export class HomeComponent implements OnInit {
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.getUsers().subscribe((users) => {
-      this.users = users;
-    });
+    // check if users data is in local storage
+    const usersData = localStorage.getItem('usersData');
+    if (usersData) {
+      this.users = JSON.parse(usersData);
+    } else {
+      this.userService.getUsers().subscribe((users) => {
+        this.users = users;
+        // save users data to local storage
+        localStorage.setItem('usersData', JSON.stringify(this.users));
+      });
+    }
+
+    // check if searched users data is in local storage
+    const searchedUsersData = localStorage.getItem('searchedUsersData');
+    if (searchedUsersData) {
+      this.searchedUsers = JSON.parse(searchedUsersData);
+    }
   }
 
   searchUser(username: string) {
@@ -23,6 +37,11 @@ export class HomeComponent implements OnInit {
       this.userService.getUser(username).subscribe(
         (user) => {
           this.searchedUsers.push(user);
+          // save searched users data to local storage
+          localStorage.setItem(
+            'searchedUsersData',
+            JSON.stringify(this.searchedUsers)
+          );
         },
         (error) => {
           alert('User not found');
@@ -35,6 +54,11 @@ export class HomeComponent implements OnInit {
     const index = this.searchedUsers.indexOf(user);
     if (index > -1) {
       this.searchedUsers.splice(index, 1);
+      // save searched users data to local storage
+      localStorage.setItem(
+        'searchedUsersData',
+        JSON.stringify(this.searchedUsers)
+      );
     }
   }
 }
