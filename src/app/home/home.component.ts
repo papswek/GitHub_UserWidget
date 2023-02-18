87f15d3a664dc951a69e39f4794e57939d99314e
papswek <97041912+papswek@users.noreply.github.com>
2023-02-19 03:01:07 +0800

    4 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,19 +8,49 @@ import { UserService } from '../user.service';
 })
 export class HomeComponent implements OnInit {
   users;
+  searchedUsers: any[] = [];
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
     this.userService.getUsers().subscribe((users) => {
       this.users = users;
-      // console.table(this.users);
     });
-
-    // this.users = Object.values(data);
   }
 
-  searchUsers(query: string) {
-    this.users = this.userService.searchUser(query);
+  searchUser(username: string) {
+    if (username.trim() !== '') {
+      this.userService.getUser(username).subscribe(
+        (user) => {
+          this.searchedUsers.push(user);
+        },
+        (error) => {
+          console.log('Error fetching user data:', error);
+        }
+      );
+    }
   }
+
+  removeUser(user: any) {
+    const index = this.searchedUsers.indexOf(user);
+    if (index > -1) {
+      this.searchedUsers.splice(index, 1);
+    }
+  }
+
+  // searchUser(username: string) {
+  //   if (username.trim() === '') {
+  //     this.searchedUser = null;
+  //   } else {
+  //     this.userService.getUser(username).subscribe((user) => {
+  //       this.searchedUser = user;
+  //       this.avatar = user.avatar_url;
+  //       this.name = user.name;
+  //       this.login = user.login;
+  //       this.company = user.company;
+  //       this.followers = user.followers;
+  //       this.publicRepos = user.public_repos;
+  //     });
+  //   }
+  // }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,40 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+
 
 @Injectable()
 export class UserService {
   private apiUrl = 'https://api.github.com/users';
-  private apiUsersUrl = 'https://api.github.com';
-  searchQuery: string = '';
+
   constructor(private http: HttpClient) {}
 
   /* Get all users */
-
   getUsers() {
     return this.http.get(`${this.apiUrl}?per_page=10`);
   }
   getUser(username: string) {
     return this.http.get(`${this.apiUrl}/${username}`);
   }
-
-  searchUser(query: string) {
-    return this.http.get(`${this.apiUsersUrl}/search/users?q=${query}`);
-  }
-
-  // searchUser(query: string): Observable<any> {
-  //   return this.http.get<any>(`${this.apiUrl}/search/users?q=${query}`);
-  // }
-  // searchQuery: string = '';
-  // users: any[] = [];
-
-  // constructor(private http: HttpClient) { }
-
-  // search() {
-  //   const apiUrl = `https://api.github.com/search/users?q=${this.searchQuery}`;
-
-  //   this.http.get(apiUrl).subscribe((response: any) => {
-  //     this.users = response.items;
-  //   });
-  // }
 }
