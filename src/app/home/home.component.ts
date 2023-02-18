2a82cd520b06ae3451396fcf30802c4160d13f99
papswek <97041912+papswek@users.noreply.github.com>
2023-02-19 04:02:49 +0800

    4 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,7 +25,7 @@ export class HomeComponent implements OnInit {
           this.searchedUsers.push(user);
         },
         (error) => {
-          console.log('Error fetching user data:', error);
+          alert('User not found');
         }
       );
     }
@@ -37,20 +37,4 @@ export class HomeComponent implements OnInit {
       this.searchedUsers.splice(index, 1);
     }
   }
-
-  // searchUser(username: string) {
-  //   if (username.trim() === '') {
-  //     this.searchedUser = null;
-  //   } else {
-  //     this.userService.getUser(username).subscribe((user) => {
-  //       this.searchedUser = user;
-  //       this.avatar = user.avatar_url;
-  //       this.name = user.name;
-  //       this.login = user.login;
-  //       this.company = user.company;
-  //       this.followers = user.followers;
-  //       this.publicRepos = user.public_repos;
-  //     });
-  //   }
-  // }
 }
